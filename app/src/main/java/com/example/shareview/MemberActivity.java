ab424b846153ba35e4ea4118910eb7b9afa1a29e
[liam-lia] <[west819.mnymk.9056@ezweb.ne.jp]>
2021-11-15 15:19:34 +0900

    登録画面(MemberActivity)を追加してます。

---
+++ app/src/main/java/com/example/shareview/MemberActivity.java
@@ -0,0 +1,30 @@
+package com.example.shareview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MemberActivity extends AppCompatActivity {
+
+    private EditText text_name, text_mail, text_pass, text_repass;
+    private Button touroku_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member);
+
+        text_name = (EditText) findViewById(R.id.text_name);
+        text_mail = (EditText) findViewById(R.id.text_mail);
+        text_pass = (EditText) findViewById(R.id.text_pass);
+        text_repass = (EditText) findViewById(R.id.text_repass);
+        touroku_button = (Button) findViewById(R.id.touroku_button);
+    }
+
+    public void onTourokuButtonClick(View view) {
+
+    }
+
+}
